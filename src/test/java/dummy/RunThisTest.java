9aa806140607ed36f09628ce6b47a38f0f06f060
Bhaskar <bhaskarreborn@gmail.com>
2020-01-25 14:40:11 +0530

    Added comment

--- src/test/java/dummy/RunThisTest.java
+++ src/test/java/dummy/RunThisTest.java
@@ -4,7 +4,7 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class RunThisTest {
-
+//comments added
 	@Test
 	public void log() {
 		Reporter.log("This is a dummy project.", true);
